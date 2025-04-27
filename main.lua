d873091d0c8f84a52fb346a26049d7f2093b7e6c
Hekepoiu <decnovpk92805@gmail.com>
2025-04-27 22:17:20 +0800

    Init

---
+++ main.lua
@@ -0,0 +1,8 @@
+function love.load()
+    love.window.setTitle("Hello World!")
+end
+function love.draw()
+    love.graphics.print("Hello World!", 400, 300)
+    love.graphics.circle("fill", 400, 300, 100)
+    local t = {};
+end
